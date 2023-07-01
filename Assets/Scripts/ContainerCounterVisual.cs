60e2c5ec0413c48ac450a000104771c424a7cb96
Noël Mugnier <noel.mugnier@outlook.com>
2023-07-01 22:14:21 +0200

    Added Container counter tray animation

---
+++ Assets/Scripts/ContainerCounterVisual.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ContainerCounterVisual : MonoBehaviour
+{
+    [SerializeField] private ContainerCounter containerCounter;
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        containerCounter.OnObjectProduced += OnObjectProduced; 
+    }
+
+    private void OnObjectProduced(object sender, OnObjectProducedArgs e)
+    {
+        _animator.SetTrigger("OpenClose");
+    }
+}
--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -15,3 +15,18 @@ public class PlayerAnimator : MonoBehaviour
         _animator.SetBool("IsWalking", player?.GetComponent<MoveHandler>()?.IsMoving ?? false);
     }
 }
+public class VisualContainerCounterAnimator : MonoBehaviour
+{
+    [SerializeField] private Player player;
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        _animator.SetBool("IsWalking", player?.GetComponent<MoveHandler>()?.IsMoving ?? false);
+    }
+}
